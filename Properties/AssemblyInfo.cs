63f202b46e7c772bf47b953093b37c34dd0cd4b5
Thiago Robles <thiagorobles3@gmail.com>
2025-03-11 17:11:40 -0300

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("TecnologyAPP")]
+[assembly: AssemblyDescription("Aplicacion desarollada para gestionar taller de service")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tecnology Digital")]
+[assembly: AssemblyProduct("Gestor-Pedidos-Tecnology")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("Thiago Robles")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("9838408d-1893-4918-9a40-9e7b86ed8ec7")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
---
+++ README.md
@@ -0,0 +1,2 @@
+# Sistema-Gestion-Taller
+ 
